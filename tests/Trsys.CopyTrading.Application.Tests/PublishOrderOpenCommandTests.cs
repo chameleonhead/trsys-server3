9a28576652267c1a18a2279965d66ab4d4bacce8
Yuto Nagano <chamelion@hotmail.co.jp>
2021-10-07 04:48:29 +0900

    edit test code

--- tests/Trsys.CopyTrading.Application.Tests/PublishOrderOpenCommandTests.cs
+++ tests/Trsys.CopyTrading.Application.Tests/PublishOrderOpenCommandTests.cs
@@ -1,6 +1,5 @@
 using EventFlow;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Trsys.CopyTrading.Application.Write;
@@ -12,7 +11,20 @@ namespace Trsys.CopyTrading.Application.Tests
     public class PublishOrderOpenCommandTests
     {
         [TestMethod]
-        public async Task Success()
+        public async Task SuccessWithoutSubscriber()
+        {
+            using var resolver = EventFlowOptions
+                .New
+                .UseApplication()
+                .CreateResolver();
+            var bus = resolver.Resolve<ICommandBus>();
+            var unknownDistributionGroupId = DistributionGroupId.New;
+            var result = await bus.PublishAsync(new PublishOrderOpenCommand(CopyTradeId.New, unknownDistributionGroupId, ForexTradeSymbol.ValueOf("USDJPY"), OrderType.Buy), CancellationToken.None);
+            Assert.IsTrue(result.IsSuccess);
+        }
+
+        [TestMethod]
+        public async Task SuccessWithASubscriber()
         {
             using var resolver = EventFlowOptions
                 .New
@@ -20,8 +32,11 @@ namespace Trsys.CopyTrading.Application.Tests
                 .CreateResolver();
             var bus = resolver.Resolve<ICommandBus>();
 
-            var distributionGroupId = DistributionGroupId.New;
-            var result = await bus.PublishAsync(new PublishOrderOpenCommand(CopyTradeId.New, distributionGroupId, ForexTradeSymbol.ValueOf("USDJPY"), OrderType.Buy), CancellationToken.None);
+            var accountId = AccountId.New;
+            var knownDistributionGroupId = DistributionGroupId.New;
+            var result = await bus.PublishAsync(new AddSubscriberCommand(knownDistributionGroupId, accountId, TradeQuantity.Percentage(98)), CancellationToken.None);
+            Assert.IsTrue(result.IsSuccess);
+            result = await bus.PublishAsync(new PublishOrderOpenCommand(CopyTradeId.New, knownDistributionGroupId, ForexTradeSymbol.ValueOf("USDJPY"), OrderType.Buy), CancellationToken.None);
             Assert.IsTrue(result.IsSuccess);
         }
     }
