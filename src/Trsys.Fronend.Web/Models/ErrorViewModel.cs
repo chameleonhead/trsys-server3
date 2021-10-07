438e2237432b5ce0ad4cb9b916a793bac2fb788c
Yuto Nagano <chamelion@hotmail.co.jp>
2021-10-07 16:47:34 +0900

    add frontend project

---
+++ src/Trsys.Fronend.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trsys.Fronend.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
